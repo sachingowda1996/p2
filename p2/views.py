5985df1e204b622d5f1ea85b43e7eb560bee896c
unknown <sachinkn2019@gmail.com>
2020-07-17 21:00:17 +0530

    templates added

--- p2/views.py
+++ p2/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("hello world")
 
 def home(request):
-    return HttpResponse("<h1> welcome to home page </h1>")    
+    return HttpResponse("<h1> welcome to home page </h1>") 
+
+def html_demo1(request):
+    return render(request,"sample.html") 
+
+def html_demo2(request):
+    return render(request,"sample1.html")          
